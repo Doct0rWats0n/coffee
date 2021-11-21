1b72a1741541ef95b3fe0fa1aa41517023d6aa2c
siri <lolipopnapalochke2281337@gmail.com>
2021-11-21 14:27:47 +0500

    Улучшенная версия без возможности редактирования

--- main.py
+++ main.py
@@ -1,26 +1,37 @@
-from PyQt5.QtWidgets import QApplication, QMainWindow
-from PyQt5.QtSql import QSqlDatabase, QSqlTableModel
-from PyQt5 import uic
+from PyQt5.QtWidgets import QApplication, QMainWindow, QTableWidget, QTableWidgetItem, QAbstractItemView
 import sys
+import sqlite3
+from PyQt5 import uic
 
 
 class Window(QMainWindow):
     def __init__(self):
         super().__init__()
-        uic.loadUi('main.ui', self)
+
+        self.base = sqlite3.connect('coffee.sqlite')
+        self.cur = self.base.cursor()
         self.initUi()
 
     def initUi(self):
-        self.setGeometry(400, 400, 400, 400)
+        uic.loadUi('main.ui', self)
+        self.table.setEditTriggers(QAbstractItemView.NoEditTriggers)
+        self.update_values()
 
-        db = QSqlDatabase.addDatabase('QSQLITE')
-        db.setDatabaseName('coffee.sqlite')
-        db.open()
+    def update_values(self):
+        data = self.cur.execute("SELECT * FROM coffee").fetchall()
+        if not data:
+            return
+        self.titles = [desc[0] for desc in self.cur.description]
 
-        db_data = QSqlTableModel(self, db)
-        db_data.setTable('coffee')
-        db_data.select()
-        self.table.setModel(db_data)
+        self.table.setRowCount(len(data))
+        self.table.setColumnCount(len(data[0]))
+        self.table.setHorizontalHeaderLabels(self.titles)
+        for index, value in enumerate(data):
+            for base_index, base_value in enumerate(value):
+                self.table.setItem(index, base_index, QTableWidgetItem(str(base_value)))
+        self.table.resizeColumnsToContents()
+
+        self.modified = {}
 
 
 if __name__ == '__main__':
